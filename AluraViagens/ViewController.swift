13a057f8495cb4b2ba7ca82f2e80f35eed08d01f
ThalesJC <thales.soul96@gmail.com>
2024-01-03 17:17:40 -0300

    cria plano de fundo do header

--- AluraViagens/ViewController.swift
+++ AluraViagens/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var viagensTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        viagensTableView.dataSource = self
         // Do any additional setup after loading the view.
     }
 
-
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        cell.textLabel?.text = "Viagem \(indexPath.row + 1)"
+        return cell
+    }
+    
+}
