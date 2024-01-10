93f7bdfd9f7a907250f16175123bf8bb67fea4cf
ThalesJC <thales.soul96@gmail.com>
2024-01-10 16:03:13 -0300

    finaliza o app

--- AluraViagens/View/Cells/ViagemTableViewCell.swift
+++ AluraViagens/View/Cells/ViagemTableViewCell.swift
@@ -8,16 +8,36 @@
 import UIKit
 
 class ViagemTableViewCell: UITableViewCell {
+    // MARK: IBOutlets
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
+    @IBOutlet weak var backgroundViewCell: UIView!
+    @IBOutlet weak var viagemImage: UIImageView!
+    @IBOutlet weak var tituloViagemLabel: UILabel!
+    @IBOutlet weak var subtituloViagemLabel: UILabel!
+    @IBOutlet weak var diariaViagemLabel: UILabel!
+    @IBOutlet weak var precoSemDescontoLabel: UILabel!
+    @IBOutlet weak var precoViagemLabel: UILabel!
+    @IBOutlet weak var statusCancelamentoViagemLabel: UILabel!
     
+    func configuraCelula(_ viagem: Viagem?) {
+        viagemImage.image = UIImage(named: viagem?.asset ?? "")
+        tituloViagemLabel.text = viagem?.titulo
+        subtituloViagemLabel.text = viagem?.subtitulo
+        precoViagemLabel.text = "R$ \(viagem?.preco ?? 0.00)"
+        
+        let atributoString: NSMutableAttributedString = NSMutableAttributedString(string: "R$ \(viagem?.precoSemDesconto ?? 0.00)")
+        atributoString.addAttribute(NSAttributedString.Key.strikethroughStyle, value: 1, range: NSMakeRange(0, atributoString.length))
+        precoSemDescontoLabel.attributedText = atributoString
+        
+        if let numeroDeDias = viagem?.diaria, let numeroDeHospedes = viagem?.hospedes {
+            let diarias = numeroDeDias == 1 ? "Diaria" : "Diarias"
+            let hospedes = numeroDeHospedes == 1 ? "Pessoa" : "Pessoas"
+            
+            diariaViagemLabel.text = "\(numeroDeDias) \(diarias) - \(numeroDeHospedes) \(hospedes)"
+        }
+        
+        DispatchQueue.main.async {
+            self.backgroundViewCell.addSombra()
+        }
+    }
 }
